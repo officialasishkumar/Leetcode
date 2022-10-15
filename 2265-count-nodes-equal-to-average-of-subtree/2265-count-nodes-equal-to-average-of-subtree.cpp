7858fffda31d57a8fed06ef59189e51b1bbeaefd
Asish Kumar <87874775+officialasishkumar@users.noreply.github.com>
2022-10-15 10:26:55 +0530

    Time: 59 ms (12.11%), Space: 11.9 MB (86.62%) - LeetHub

--- 2265-count-nodes-equal-to-average-of-subtree/2265-count-nodes-equal-to-average-of-subtree.cpp
+++ 2265-count-nodes-equal-to-average-of-subtree/2265-count-nodes-equal-to-average-of-subtree.cpp
@@ -10,19 +10,25 @@
  * };
  */
 class Solution {
-    int ans = 0;
+    int ans{};
 public:
-    pair<int, int> solve(TreeNode* root){
-        if(!root) return {0, 0}; // In case we hit a NULL node we have to return sum and size to be 0.
-        auto left = solve(root->left); // Returns the sum and size of the left subtree.
-        auto right = solve(root->right); // Returns the sum and size of the right subtree.
-        int sum = (left.first + right.first + root->val); // We add all the three values i.e. the sum of the left and right subtree and the value of the current node.
-        int n = (left.second + right.second + 1); // We calculate the size of the entire subtree including the parent node. 
-        if((sum/n) == root->val) ans++; // If the condition is passed we increment our ans variable. 
-        return {sum, n}; // At the end we return the sum and the size of the tree.
+    int calc(TreeNode* root, int &sz){
+        if(!root) return 0;
+        sz++;
+        int left = calc(root->left, sz);
+        int right = calc(root->right, sz);
+        return left + right + root->val;
+    }
+    void preOrder(TreeNode* root){
+        if(!root) return;
+        int sz = 0;
+        int avg = (calc(root, sz)/sz);
+        if(avg == root->val) ans++;
+        preOrder(root->left);
+        preOrder(root->right);
     }
     int averageOfSubtree(TreeNode* root) {
-        auto pr = solve(root);
+        preOrder(root);
         return ans;
     }
 };
