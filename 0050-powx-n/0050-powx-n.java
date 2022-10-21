137582b70103c46812bd07333da4d6bfa5cbae7b
Virendra Maurya <75972251+Virendra115@users.noreply.github.com>
2022-10-21 19:16:21 +0530

    Time: 2 ms (15.49%), Space: 42 MB (80.57%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.java
@@ -0,0 +1,13 @@
+class Solution {
+    public double myPow(double x, int n) {
+        if(n==0) return 1;
+        if(n<0) return 1.0/pow(x,-1L*n);
+        return pow(x,n);
+    }
+    double pow(double x,long n){
+        if(n==1) return x;
+        double res = pow(x,n/2);
+        if(n%2==0) return res*res;
+        return res*res*x;
+    }
+}
