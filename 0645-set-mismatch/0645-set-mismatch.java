2c18eab64cd7747ff4f64384656ca08e2332c0f1
Virendra Maurya <75972251+Virendra115@users.noreply.github.com>
2022-10-23 09:04:43 +0530

    Time: 28 ms (16.81%), Space: 55.4 MB (14.89%) - LeetHub

---
+++ 0645-set-mismatch/0645-set-mismatch.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] findErrorNums(int[] nums) {
+        int[] ans = new int[2];
+        int xor = 0;
+        Set<Integer> set = new HashSet<>();
+        for(int i=0;i<nums.length;i++){
+            if(set.contains(nums[i])){
+                ans[0]= nums[i];
+            }else{
+                set.add(nums[i]);
+                xor ^= nums[i];
+            }
+            xor ^= i+1;
+        }
+        ans[1] = xor;
+        return ans;
+    }
+}
