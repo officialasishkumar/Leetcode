003b906e32ceaf586ffd61033cb48ffb82171bcd
iam_chandra3 <chandrasekhar_v@srmap.edu.in>
2022-10-24 20:34:32 +0530

    Added two-sum solution in python

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,6 @@
+def twoSum( nums, target):
+    for i in range(len(nums)):
+        for j in range(i+1, len(nums)):
+            if nums[i] + nums[j] == target:
+                return [i, j]
+print(*twoSum(([3,3]),6))
