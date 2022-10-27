4bcc103c4f0c2138814cbbe27099776f76580ded
Virendra Maurya <75972251+Virendra115@users.noreply.github.com>
2022-10-27 11:24:37 +0530

    Time: 42 ms (66.67%), Space: 43.7 MB (37.58%) - LeetHub

--- 0835-image-overlap/0835-image-overlap.java
+++ 0835-image-overlap/0835-image-overlap.java
@@ -1,12 +1,12 @@
 class Solution {
     public int largestOverlap(int[][] img1, int[][] img2) {
         int ans = 0,n = img1.length;
-        for(int x = -n; x<n; x++){
-            for(int y = -n; y< n; y++){
+        for(int x = -n; x < n; x++){
+            for(int y = -n; y < n; y++){
                 int cur = 0;
-                for(int i=0; i<n; i++){
-                    for(int j=0;j<n; j++){
-                        if(i+x>=0&&i+x<n&&j+y>=0&&j+y<n&&img1[i][j]==1&&img2[i+x][j+y]==1) cur++;
+                for(int i = Math.max(0, 0 - x); i < Math.min(n, n - x); i++){
+                    for(int j = Math.max(0, 0 - y); j < Math.min(n, n - y); j++){
+                        if(img1[i][j] == 1 && img2[i+x][j+y] == 1) cur++;
                     }
                 }
                 ans = Math.max(ans, cur);
