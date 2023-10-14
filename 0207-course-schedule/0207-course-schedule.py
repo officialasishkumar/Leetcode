9c34e77062d560bea397d1a45596024aa41b6699
Dayita Chaudhuri <dayitachaudhuri.tech@gmail.com>
2023-10-14 12:39:28 +0530

    Time: 102 ms (40.46%), Space: 19.1 MB (58.46%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -12,16 +12,17 @@ class Solution:
             if indegree[i] == 0:
                 available.append(i)
         
-        while available:
-            temp = []
-            for course in available:
-                for nextCourse in req[course]:
-                    indegree[nextCourse] -= 1
-                    if indegree[nextCourse] == 0:
-                        temp.append(nextCourse)
-            available = temp
+        def dfs(curr):
+            for course in req[curr]:
+                indegree[course] -= 1
+                if indegree[course] == 0:
+                    dfs(course)
         
+        for course in available:
+            dfs(course)
+
         for i in range(0, numCourses):
             if indegree[i] > 0:
                 return False
+                
         return True
