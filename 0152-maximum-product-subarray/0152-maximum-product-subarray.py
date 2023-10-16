a345688b9c2aeeb5330573d4d176924eb2872e2f
Dayita Chaudhuri <dayitachaudhuri.tech@gmail.com>
2023-10-16 23:39:15 +0530

    Time: 88 ms (16.06%), Space: 16.8 MB (41.52%) - LeetHub

---
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.py
@@ -0,0 +1,21 @@
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        n = len(nums)
+        leftProd = 1
+        leftMax = -9999999999
+        rightProd = 1
+        rightMax = -9999999999
+        maxValue = nums[0]
+
+        for i in range(0,n):
+            leftProd *= nums[i]
+            leftMax = max(leftProd, leftMax)
+            rightProd *= nums[n-i-1]
+            rightMax = max(rightProd, rightMax)
+            maxValue = max(nums[i], maxValue)
+            if leftProd == 0:
+                leftProd = 1
+            if rightProd == 0:
+                rightProd = 1
+        
+        return max(max(leftMax, rightMax), maxValue)
