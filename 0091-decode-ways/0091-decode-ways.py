00e35b555eeccb47c7aafd6ed5eff47f207659ff
Dayita Chaudhuri <dayitachaudhuri.tech@gmail.com>
2023-11-10 00:32:59 +0530

    Time: 46 ms (19.3%), Space: 16.1 MB (84.71%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.py
+++ 0091-decode-ways/0091-decode-ways.py
@@ -2,22 +2,18 @@ class Solution:
     def numDecodings(self, s: str) -> int:
 
         n = len(s)
-        dp = [-1]*n
+        dp = [-1]*(n+1)
+        dp[n] = 1
 
-        def helper(i):
-            if i >= n:
-                return 1
+        for i in range(n-1,-1,-1):
             if s[i] == "0":
-                return 0
-            if dp[i] != -1:
-                return dp[i]
-            if i < n-1 and int(s[i:i+2]) <= 26:
-                dp[i] = helper(i+1) + helper(i+2)
+                dp[i] = 0
+            elif i < n-1 and int(s[i:i+2]) <= 26:
+                dp[i] = dp[i+1] + dp[i+2]
             else:
-                dp[i] = helper(i+1)
-            return dp[i]
+                dp[i] = dp[i+1]
         
-        return helper(0)
+        return dp[0]
 
 
         
